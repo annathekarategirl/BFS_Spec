e43232ce06d38862d5eddd0b81adcb4489d95c7d
Anna Moore <annathekarategirl@gmail.com>
2026-04-15 14:05:30 -0400

    loop

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ const calcBoxDiv=document.getElementById("calcBox");
 
  }
 
+  class Node{
+
+ }
+
  class Grid{
     constructor()//height,width)
     {
@@ -17,14 +21,13 @@ const calcBoxDiv=document.getElementById("calcBox");
         // this.width=width
     } 
     makeGrid(height,width){
+        var array=[]
         for(let i=0;i<height;i++){
+            array.push([])
             for (let j=0;j<width;i++){
-                
+                array[i].push(new Node())
             }
         }
     }
 }
 
- class Node{
-
- }
