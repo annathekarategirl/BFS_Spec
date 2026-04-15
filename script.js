ed6286e930111796b5278cfc3cf40a0161963461
Anna Moore <annathekarategirl@gmail.com>
2026-04-15 14:03:03 -0400

    f

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 const gridDiv=document.getElementById('grid');
 const calcBoxDiv=document.getElementById("calcBox");
+
+
+ class Algorithm{
+    //Abstract class
+ }
+
+ class BFS extends Algorithm{
+
+ }
+
+ class Grid{
+    constructor()//height,width)
+    {
+        // this.height=height
+        // this.width=width
+    } 
+    makeGrid(height,width){
+        for(let i=0;i<height;i++){
+            for (let j=0;j<width;i++){
+                
+            }
+        }
+    }
+}
+
+ class Node{
+
+ }
