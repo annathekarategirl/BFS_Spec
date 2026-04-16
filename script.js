534f5ee54a58c542fd750a9b28cc0c0733786ad6
Anna Moore <annathekarategirl@gmail.com>
2026-04-16 13:45:41 -0400

    um

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ const calcBoxDiv=document.getElementById("calcBox");
  }
 
   class Node{
-     constructor(type){
+     constructor(type, tableData, coordinates){
         this.type=type
+        this.tableData=tableData
      }
  }
 
@@ -30,11 +31,23 @@ const calcBoxDiv=document.getElementById("calcBox");
             tableRow.id="Row "+i
             gridDiv.appendChild(tableRow)
             for (let j=0;j<width;j++){
-                array[i].push(new Node("normal"))
-                let tableData= document.createElement("td")
-                tableData.id="Row-"+i+" Data-"+j
-                tableRow.appendChild(tableData)
-                tableData.innerHTML="<img src='whitesquare.png' style='height:50px;width:50px'>"
+                array[i].push(new Node("normal",document.createElement("td"),[i,j]))
+                array[i][j].tableData.id="Row-"+i+" Data-"+j
+                tableRow.appendChild(array[i][j].tableData)
+                array[i][j].tableData.innerHTML="<img src='whitesquare.png' style='height:50px;width:50px'>"
+                array[i][j].tableData.addEventListener("click",function(){
+                    console.log("hi");
+                    console.log(array[i][j].type)
+                    if(array[i][j].type=="normal"){
+                        console.log("type works")
+                        array[i][j].type='wall';
+                        console.log(array[i][j].type)
+                        array[i][j].tableData.innerHTML="<img src='blacksquare.png' style='height:50px;width:50px'>"};
+                        console.log(array[i][j].tableData.innerHTML)
+                    if(array[i][j].type=="wall")
+                            {array[i][j].type='normal';
+                            array[i][j].tableData.innerHTML="<img src='whitesquare.png' style='height:50px;width:50px'>"}
+                    else{console.log("test")};})
             }
         }
         console.log(array)
@@ -42,4 +55,4 @@ const calcBoxDiv=document.getElementById("calcBox");
 }
     
     let grid= new Grid
-    grid.makeGrid(6,7)
+    grid.makeGrid(5,5)
